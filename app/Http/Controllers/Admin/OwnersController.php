b38e9ef5b593459bd0857a48179e3fa040b3b145
kanekonanami <n-kaneko@jutjoy.com>
2022-12-18 13:41:13 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner;
+use Illuminate\Support\Facades\Hash;
 
 class OwnersController extends Controller
 {
@@ -43,7 +44,20 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index')
+        ->with('message', 'オーナー登録を実施しました');
     }
 
     /**
