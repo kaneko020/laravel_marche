f6ced2eeaa8fe9449f2fcd3c66ce21b1d8ced9c1
KanekoNanami <n-kaneko@jutjoy.com>
2023-06-11 13:02:02 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Owner;
 use App\Models\Image;
-use App\Models\SecondaryCategory;
+use App\Models\PrimaryCategory;
+use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
@@ -39,7 +40,19 @@ class ProductController extends Controller
 
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+            ->select('id', 'name')
+            ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+            ->select('id' ,'title', 'filename')
+            ->orderBy('updated_at', 'desc')
+            ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+            ->get();
+        
+        return view('owner.products.create', compact('shops', 'images', 'categories'));
     }
 
     public function store(Request $request)
