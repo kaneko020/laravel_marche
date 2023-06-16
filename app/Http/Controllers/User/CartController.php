60d82c92ce851ffb55a9634f0b0c7a9dd801b27d
KanekoNanami <n-kaneko@jutjoy.com>
2023-06-17 08:39:14 +0900

    cart add

---
+++ app/Http/Controllers/User/CartController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
+    public function add(Request $request)
+    {
+        $itemInCart = Cart::where('user_id', Auth::id())
+            ->where('product_id', $request->product_id)
+            ->first();
+
+        if ($itemInCart ){
+            $itemInCart->quantity += $request->quantity;
+            $itemInCart->save();
+        } else {
+            Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity,
+            ]);
+        }
+    }
+}
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -52,22 +52,26 @@
                       <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">{{ $product->category->name }}</h2>
                       <h1 class="mb-4 text-gray-900 text-3xl title-font font-medium">{{ $product->name }}</h1>
                       <p class="mb-12 leading-relaxed">{{ $product->information }}</p>
-                      <div class="flex justify-end items-center mb-6">
-                        <span class="mr-3">数量</span>
-                        <div class="relative">
-                          <select name="quantity" class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                            @for($i = 1; $i <= $quantity; $i++)
-                              <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
-                          </select>
+                      <form method="post" action="{{ route('user.cart.add') }}">
+                        @csrf
+                        <div class="flex justify-end items-center mb-6">
+                          <span class="mr-3">数量</span>
+                          <div class="relative">
+                            <select name="quantity" class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                              @for($i = 1; $i <= $quantity; $i++)
+                                <option value="{{ $i }}">{{ $i }}</option>
+                              @endfor
+                            </select>
+                          </div>
                         </div>
-                      </div>
-                      <div class="flex justify-between items-end">
-                        <div class="mr-4">
-                          <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm text-gray-700">円(税込)</span>
+                        <div class="flex justify-between items-end">
+                          <div class="mr-4">
+                            <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm text-gray-700">円(税込)</span>
+                          </div>
+                          <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                          <input type="hidden" name="product_id" value="{{ $product->id }}">
                         </div>
-                        <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
-                      </div>
+                      </form>
                     </div>
                   </div>
                   <div class="border-t border-gray-400 my-8 pt-8">
