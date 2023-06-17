573e77fd64b666d58f000fde575ea9d9c716355c
KanekoNanami <n-kaneko@jutjoy.com>
2023-06-17 15:05:20 +0900

    検索

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -114,4 +114,13 @@ class Product extends Model
             return $query->orderBy('created_at', 'asc');
         }
     }
+
+    public function scopeSelectCategory($query, $categoryId)
+    {
+        if ($categoryId !== '0') {
+            return $query->where('secondary_category_id', $categoryId);
+        } else {
+            return;
+        }
+    }
 }
