9f3fbdcf490541859399d556f128c1bea1e6880d
KanekoNanami <n-kaneko@jutjoy.com>
2023-06-17 09:59:25 +0900

    stripe 在庫処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Cart;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 
 class CartController extends Controller
 {
@@ -64,16 +65,35 @@ class CartController extends Controller
 
         $lineItems = [];
         foreach ($products as $product) {
-            $lineItem = [
-                'name' => $product->name,
-                'discription' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
-            ];
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)
+                ->sum('quantity');
 
-            array_push($lineItems, $lineItem);
+            if ($product->pivot->quantity > $quantity) {
+                return redirect()->route('user.cart.index')
+                    ->with('message', '在庫が足りません');
+            } else {
+                $lineItem = [
+                    'name' => $product->name,
+                    'discription' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+                ];
+
+                array_push($lineItems, $lineItem);
+            }
         }
+
+        foreach ($products as $product) {
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1,
+            ]);
+        }
+
+        dd('test');
 
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
